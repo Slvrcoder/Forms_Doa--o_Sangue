a988d314a0d32f634a9597e2bf5cc289523cc293
Slvrcoder <matheus_silveira9@sesisenai.org.br>
2026-04-06 09:15:53 -0300

    ADD - Validações e funções

--- script.js
+++ script.js
@@ -8,9 +8,15 @@ form.addEventListener('submit', function(e) {
 
     let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
+    let idade = document.getElementById('idade').value;
+    let peso = document.getElementById('peso').value;
+    let tipoSangue = document.getElementById('tipoSangue').value;
 
     document.getElementById('erroNome').textContent = '';
     document.getElementById('erroEmail').textContent = '';
+    document.getElementById('erroIdade').textContent = '';
+    document.getElementById('erroPeso').textContent = '';
+    document.getElementById('erroTipoSangue').textContent = '';
 
     if (nome.length < 10) {
         document.getElementById('erroNome').textContent =
@@ -24,11 +30,26 @@ form.addEventListener('submit', function(e) {
         valido = false;
     }
 
+    if (idade < 16 || idade > 120) {
+        document.getElementById('erroIdade').textContent =
+            'Você deve ser maior de 16 anos para doar sangue.';
+        valido = false;
+    }
+    if (peso < 50) {
+        document.getElementById('erroPeso').textContent =
+            'O peso mínimo para doação é de 50 kg.';
+        valido = false;
+    }
+    
+
     if (valido) {
         document.getElementById('resultado').innerHTML = `
         <h3>Dados enviados com sucesso!</h3>
         Nome: ${nome} <br>
-        Email: ${email} 
+        Email: ${email} <br>
+        Idade: ${idade} <br>
+        Peso: ${peso} kg <br>
+        Tipo Sanguíneo: ${tipoSangue} <br>
         `;
     }
 
