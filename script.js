6d44c42337b38ba5622a781c81ac18b90dc1660d
Slvrcoder <matheus_silveira9@sesisenai.org.br>
2026-04-06 08:37:24 -0300

    ADD - Primeira validação concluída

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ form.addEventListener('submit', function(e) {
 
     e.preventDefault();
 
-    let nome = document.getElementById('nome').value;
-
     let valido = true;
 
-    document.getElementById('errorNome').textContent = '';
+    let nome = document.getElementById('nome').value;
 
-
-
-
+    document.getElementById('erroNome').textContent = '';
 
     if (nome.length < 10) {
-
-        document.getElementById('errorNome').textContent = 'O nome deve conter pelo menos 10 caracteres.';
+        document.getElementById('erroNome').textContent =
+            'O nome deve conter pelo menos 10 caracteres.';
         valido = false;
+    }
+
+    if (valido) {
+        document.getElementById('resultado').innerHTML = `
+        <h3>Dados enviados com sucesso!</h3>
+        Nome: ${nome}
+        `;
+    }
+
+});
